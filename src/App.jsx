27658588c55cd2471b4b56a5efe64a2dbae7aca8
Felipe Rodrigues <99354371+felipfr@users.noreply.github.com>
2023-02-24 21:16:37 -0300

    Criando a pasta source

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Quiz from "./Quiz";
+
+const App = () => {
+  function blockRightClick(ev) {
+    ev.preventDefault();
+  }
+
+  return (
+    <div onContextMenu={blockRightClick}>
+      <Quiz />
+    </div>
+  );
+};
+
+export default App;
